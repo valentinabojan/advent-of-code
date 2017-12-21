ea1686ad6969d3a0e555a94dd00fda9884b2796b
valentinabojan <valentina.bojan@cti.pub.ro>
2017-12-21 11:39:39 +0200

    continue solution for day 21 - part 1

--- src/main/kotlin/day_21/FractalArt.kt
+++ src/main/kotlin/day_21/FractalArt.kt
@@ -8,40 +8,94 @@ class FractalArt {
     private val OFF = "."
 
     fun partOne(rules: MutableMap<List<List<String>>, List<List<String>>>, input: List<List<String>>, iterations: Int): Int {
-        val image = input.toMutableList()
+        var image = input.toMutableList()
 
         for (i in 0 until iterations) {
-            if (image.size % 2 == 0) {
-                val smallImages = mutableListOf<List<List<String>>>()
+            val smallImages = mutableListOf<List<List<String>>>()
 
+            if (image.size % 2 == 0) {
                 for (y in 0 until image.size step 2) {
                     for (x in 0 until image.size step 2) {
-                        val smallImage = mutableListOf<MutableList<String>>()
+                        val smallImage = List(2) { arrayOfNulls<String>(2) }
 
                         smallImage[0][0] = image[y][x]
                         smallImage[0][1] = image[y][x + 1]
                         smallImage[1][0] = image[y + 1][x]
                         smallImage[1][1] = image[y + 1][x + 1]
 
-                        smallImages.add(smallImage)
+                        for (rule in rules) {
+                            if (checkRule(rule.key, smallImage.map { it.toList().requireNoNulls() })) {
+                                smallImages.add(rule.value)
+                                break
+                            }
+                        }
                     }
                 }
 
+                val newImage = mutableListOf<MutableList<String>>()
+                for (s in 0 until smallImages.size) {
+                    val row = s/2
 
+                    if (s%2 == 0) {
+                        smallImages[s].forEach{ newImage.add(it.toMutableList())}
+                    } else {
+                        for (si in 0 until 3) {
+                            newImage[3 * row + si].addAll(smallImages[s][si])
+                        }
+                    }
+                }
 
+                image = newImage.toMutableList()
 
+            } else if (image.size % 3 == 0) {
+                for (y in 0 until image.size step 3) {
+                    for (x in 0 until image.size step 3) {
+                        val smallImage = List(3) { arrayOfNulls<String>(3) }
 
+                        smallImage[0][0] = image[y][x]
+                        smallImage[0][1] = image[y][x + 1]
+                        smallImage[0][2] = image[y][x + 2]
+                        smallImage[1][0] = image[y + 1][x]
+                        smallImage[1][1] = image[y + 1][x + 1]
+                        smallImage[1][2] = image[y + 1][x + 2]
+                        smallImage[2][0] = image[y + 2][x]
+                        smallImage[2][1] = image[y + 2][x + 1]
+                        smallImage[2][2] = image[y + 2][x + 2]
 
+                        for (rule in rules) {
+                            if (checkRule(rule.key, smallImage.map { it.toList().requireNoNulls() })) {
+                                smallImages.add(rule.value)
+                                break
+                            }
+                        }
+                    }
+                }
 
+                val newImage = mutableListOf<MutableList<String>>()
+                for (s in 0 until smallImages.size) {
+                    val row = s/3
+
+                    if (s%3 == 0) {
+                        smallImages[s].forEach{ newImage.add(it.toMutableList())}
+                    } else {
+                        for (si in 0 until 4) {
+                            newImage[4 * row + si].addAll(smallImages[s][si])
+                        }
+                    }
+                }
+
+                image = newImage.toMutableList()
             }
 
-
+            println()
         }
 
+        var count = 0
+
+        image.forEach { it.forEach { if(it == ON) count++ } }
 
 
-
-        return 0
+        return count
     }
 
     private fun checkRule(rule: List<List<String>>, input: List<List<String>>): Boolean {
@@ -100,9 +154,9 @@ class FractalArt {
 }
 
 fun main(args: Array<String>) {
-    val rules = FractalArt().readFile("src/main/resources/day_21/rules.txt")
+    val rules = FractalArt().readFile("src/main/resources/day_21/rules_1.txt")
     val input = listOf(listOf(".", "#", "."), listOf(".", ".", "#"), listOf("#", "#", "#"))
-    val iterations = 5
+    val iterations = 2
 
     println(FractalArt().partOne(rules, input, iterations))
 //    println(ParticleSwarm().partTwo(positions, accelerations, velocities))
